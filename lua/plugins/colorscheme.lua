9767713c8d3dc85e238c8be4f3788176363028f2
Akito13 <akitokami13@gmail.com>
2024-07-14 10:29:40 +0700

    Changed line number coloring

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,11 @@ return {
     require("bamboo").setup({
       style = "vulgaris",
       transparent = true,
+      highlights = {
+        LineNrAbove = { fg = "#c6e2ff", bold = true },
+        LineNr = { fg = "#fff", bold = true },
+        LineNrBelow = { fg = "#ff7373", bold = true },
+      },
     })
     require("bamboo").load()
   end,
